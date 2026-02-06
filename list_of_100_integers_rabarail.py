933f67cea1db0fb5e1f58ff823d938fa6a01067b
Rajani <rajani.kami004@gmail.com>
2026-02-05 19:53:02 -0500

    Print sum of list

--- list_of_100_integers_rabarail.py
+++ list_of_100_integers_rabarail.py
@@ -4,5 +4,7 @@ Purpose: Make a list of  100 integers
 Starter code: None
 Date:02/05/2026"""
 import random
-print([random.randint(1, 10) for _ in range(100)])
+list = ([random.randint(1, 10) for _ in range(100)])
+print(list)
+print("Sum of list:", sum(list))
 
